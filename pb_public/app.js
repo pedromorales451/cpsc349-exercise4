12bef77f86865ccbe4da0f414a295020b1713493
Pedro Morales <pedrom2@csu.fullerton.edu>
2022-12-16 13:43:57 -0800

    changed react hrefs from index.html to app-index.html

--- pb_public/app.js
+++ pb_public/app.js
@@ -3,6 +3,13 @@ const PASSWORD = "4!P_031dk1a(31";
 const pb = new PocketBase("http://127.0.0.1:8090");
 const authData = await pb.admins.authWithPassword(USERNAME, PASSWORD);
 console.log(authData);
+console.log("hello there");
+console.log(pb.authStore.isValid);
+console.log(pb.authStore.token);
+console.log(pb.authStore.model.id);
+const record1 = await pb.collection("users").getOne(
+  pb.authStore.model.id
+);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   /* @__PURE__ */ React.createElement(React.StrictMode, null, /* @__PURE__ */ React.createElement(App, null), /* @__PURE__ */ React.createElement(App, null))
--- src/app.jsx
+++ src/app.jsx
@@ -6,6 +6,17 @@ const pb = new PocketBase('http://127.0.0.1:8090')
 const authData = await pb.admins.authWithPassword(USERNAME, PASSWORD)
 console.log(authData)
 
+console.log("hello there")
+
+console.log(pb.authStore.isValid);
+console.log(pb.authStore.token);
+console.log(pb.authStore.model.id);
+
+const record1 = await pb.collection('users').getOne(pb.authStore.model.id
+);
+
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
 root.render(
@@ -22,3 +33,4 @@ function App () {
     </div>
   )
 }
+
