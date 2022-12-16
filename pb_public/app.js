fe127effe67a7a4f5531bdd43796b74b68936584
Pedro Morales <pedrom2@csu.fullerton.edu>
2022-12-15 20:57:11 -0800

    updated nav bar, users can sign up now implemented

---
+++ pb_public/app.js
@@ -0,0 +1,12 @@
+const USERNAME = "pedrom2@csu.fullerton.edu";
+const PASSWORD = "4!P_031dk1a(31";
+const pb = new PocketBase("http://127.0.0.1:8090");
+const authData = await pb.admins.authWithPassword(USERNAME, PASSWORD);
+console.log(authData);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  /* @__PURE__ */ React.createElement(React.StrictMode, null, /* @__PURE__ */ React.createElement(App, null), /* @__PURE__ */ React.createElement(App, null))
+);
+function App() {
+  return /* @__PURE__ */ React.createElement("div", { className: "App" }, /* @__PURE__ */ React.createElement("h1", null, "Hello, ", authData.admin.email));
+}
---
+++ pb_public/js/signup.js
@@ -0,0 +1,19 @@
+const pb = new PocketBase('http://127.0.0.1:8090');
+
+const submitButton = document.getElementById('sign-up-submit')
+
+submitButton.addEventListener('click', async () => {
+  const userVal = document.getElementById('username').value
+  const emailVal = document.getElementById('email').value
+  const passwordVal = document.getElementById('password').value
+  const confirmPass = document.getElementById('confirm-password').value
+
+  if (!(userVal === '' || emailVal === '' || passwordVal === '' || confirmPass === '' || passwordVal !== confirmPass)) {
+    const record = await pb.collection('users').create({
+      username: userVal,
+      email: emailVal, 
+      password: passwordVal,
+      passwordConfirm: confirmPass
+    });
+  }
+});
